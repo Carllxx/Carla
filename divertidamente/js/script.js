5ceb749c3cfa3f10c03506ee842e6140076a48e8
Carllxx <159367323+Carllxx@users.noreply.github.com>
2024-08-27 16:37:36 -0300

    Add files via upload

--- divertidamente/js/script.js
+++ divertidamente/js/script.js
@@ -5,24 +5,26 @@ const alegria=document.querySelector("#alegria")
 const nojinho=document.querySelector("#nojinho")
 const tristeza=document.querySelector("#tristeza")
 const img = document.querySelector("img")
+const conteiner =document.querySelector(".central")
 
 raiva.addEventListener("click", () => {
     img.src ="imagens/01.png"
-
+    conteiner.style.backgroundColor = 'darkred'
 })
 
 alegria.addEventListener("click",() => {
     img.src ="imagens/02.png"
+ conteiner.style.backgroundColor = 'yellow'
 })
 
 nojinho.addEventListener("click", () => {
 img.src ="imagens/03.png"
-
+ conteiner.style.backgroundColor = 'darkgreen'
 } )
 
 tristeza.addEventListener("click", () => {
 img.src ="imagens/04.png"
-
+ conteiner.style.backgroundColor = 'darkblue'
 
 })
 
