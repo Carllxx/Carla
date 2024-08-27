9eec1139dfb1be89d2ba3c687043a77a326007fa
Carllxx <159367323+Carllxx@users.noreply.github.com>
2024-08-27 15:45:48 -0300

    Add files via upload

---
+++ divertidamente/js/script.js
@@ -0,0 +1,49 @@
+//DOM
+
+const raiva=document.querySelector("#raiva")
+const alegria=document.querySelector("#alegria")
+const nojinho=document.querySelector("#nojinho")
+const tristeza=document.querySelector("#tristeza")
+const img = document.querySelector("img")
+
+raiva.addEventListener("click", () => {
+    img.src ="imagens/01.png"
+
+})
+
+alegria.addEventListener("click",() => {
+    img.src ="imagens/02.png"
+})
+
+nojinho.addEventListener("click", () => {
+img.src ="imagens/03.png"
+
+} )
+
+tristeza.addEventListener("click", () => {
+img.src ="imagens/04.png"
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
